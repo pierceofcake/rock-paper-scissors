d638e19b0a0a1c90e2a9f926de73a2fae1681f15
Pierce Langdon <langdon.jp@gmail.com>
2023-02-07 17:48:04 +1100

    Build base UI and rework game logic.

--- js/script.js
+++ js/script.js
@@ -2,10 +2,93 @@
   Basic text-based rock paper scissors game. Plays to 5 rounds and keeps score.
 */
 
-const compSelection = getComputerChoice();
 let rounds = 0;
 let userScore = 0;
 let compScore = 0;
+const btnRock = document.querySelector(".rock");
+const btnPaper = document.querySelector(".paper");
+const btnScissors = document.querySelector(".scissors");
+const resultContainer = document.querySelector(".results");
+
+// while (true) {
+//   if (rounds >= 5) {
+//     alert(
+//       `Final score after 5 rounds: You scored ${userScore} point(s). The computer's score was ${compScore} point(s).`
+//     );
+//     break;
+//   }
+//   let result = game();
+
+//   if (result === 1) {
+//     userScore++;
+//   } else if (result === 0) {
+//     compScore++;
+//   }
+//   rounds++;
+// }
+
+// Player chooses rock
+
+btnRock?.addEventListener("click", () => {
+  const compSelection = getComputerChoice();
+  const result = playRound("rock", compSelection);
+
+  if (result === 1) {
+    const p = document.createElement("p");
+    p.textContent = "You win! Rock beats scissors.";
+    resultContainer?.appendChild(p);
+  } else if (result === 0) {
+    const p = document.createElement("p");
+    p.textContent = "You lose! Paper beats rock.";
+    resultContainer?.appendChild(p);
+  } else {
+    const p = document.createElement("p");
+    p.textContent = "It's a tie! You both chose rock.";
+    resultContainer?.appendChild(p);
+  }
+});
+
+// Player chooses paper
+
+btnPaper?.addEventListener("click", () => {
+  const compSelection = getComputerChoice();
+  const result = playRound("paper", compSelection);
+
+  if (result === 1) {
+    const p = document.createElement("p");
+    p.textContent = "You win! Paper beats rock.";
+    resultContainer?.appendChild(p);
+  } else if (result === 0) {
+    const p = document.createElement("p");
+    p.textContent = "You lose! Scissors beats paper.";
+    resultContainer?.appendChild(p);
+  } else {
+    const p = document.createElement("p");
+    p.textContent = "It's a tie! You both chose paper.";
+    resultContainer?.appendChild(p);
+  }
+});
+
+// Player chooses scissors
+
+btnScissors?.addEventListener("click", () => {
+  const compSelection = getComputerChoice();
+  const result = playRound("scissors", compSelection);
+
+  if (result === 1) {
+    const p = document.createElement("p");
+    p.textContent = "You win! Scissors beats paper.";
+    resultContainer?.appendChild(p);
+  } else if (result === 0) {
+    const p = document.createElement("p");
+    p.textContent = "You lose! Rock beats scissors.";
+    resultContainer?.appendChild(p);
+  } else {
+    const p = document.createElement("p");
+    p.textContent = "It's a tie! You both chose scissors.";
+    resultContainer?.appendChild(p);
+  }
+});
 
 function getComputerChoice() {
   let compChoice = Math.floor(Math.random() * 3);
@@ -19,65 +102,40 @@ function getComputerChoice() {
   }
 }
 
-function playRound(playerSelection, compSelection) {
-  let playerChoice = playerSelection.toLowerCase();
-
+function playRound(playerChoice, getComputerChoice) {
+  const compSelection = getComputerChoice;
   if (playerChoice === compSelection) {
-    alert("Tie!");
     return 2;
   } else {
     if (playerChoice === "rock") {
       if (compSelection === "scissors") {
-        alert("You win! Rock beats scissors.");
         return 1;
       } else {
-        alert("You lose! Paper beats rock.");
         return 0;
       }
     } else if (playerChoice === "paper") {
       if (compSelection === "rock") {
-        alert("You win! Paper beats rock.");
         return 1;
       } else {
-        alert("You lose! Scissors beats paper.");
         return 0;
       }
     } else if (playerChoice === "scissors") {
       if (compSelection === "paper") {
-        alert("You win! Scissors beats paper.");
         return 1;
       } else {
-        alert("You lose! Rock beats scissors.");
         return 0;
       }
     }
   }
 }
 
-function game() {
-  let userInput = prompt('Type "rock", "paper", or "scissors".');
-  let result = playRound(userInput, getComputerChoice());
+// function game() {
+//   // let userInput = prompt('Type "rock", "paper", or "scissors".');
+//   let result = playRound(userInput, getComputerChoice());
 
-  if (result === 1) {
-    return 1;
-  } else if (result === 0) {
-    return 0;
-  }
-}
-
-while (true) {
-  if (rounds >= 5) {
-    alert(
-      `Final score after 5 rounds: You scored ${userScore} point(s). The computer's score was ${compScore} point(s).`
-    );
-    break;
-  }
-  let result = game();
-
-  if (result === 1) {
-    userScore++;
-  } else if (result === 0) {
-    compScore++;
-  }
-  rounds++;
-}
+//   if (result === 1) {
+//     return 1;
+//   } else if (result === 0) {
+//     return 0;
+//   }
+// }
